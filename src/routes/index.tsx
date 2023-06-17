913828a3cda2f5cd8dd848c8adf4152d7ac4066c
hadeelalwadia <hadeelwadia@gmail.com>
2023-06-18 00:39:50 +0300

    create basic routes in page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,30 +2,38 @@ import React from "react";
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../screens/Home";
 import Error from '../screens/Error'
+import Courses from "../screens/Courses";
+import CoursDetalies from "../screens/CoursDetalies";
+import Categories from "../screens/Categories";
+import Booking from "../screens/booking/Booking";
+import Login from "../screens/auth/login";
+import ForgetPassword from "../screens/auth/ForgetPassword";
+import OPT from "../screens/auth/OPT";
+import SignUp from "../screens/auth/SignUp";
 
 const screensPath = [
-  { path: "/", element: <div /> },
+  { path: "/", element: <Home /> },
   { path: "*", element: <Error /> },
-  { path: "/courses", element: <div /> },
-  { path: "", element: <Error /> },
-  { path: "", element: <Error /> },
-  { path: "/", element: <Error /> },
-  { path: "*", element: <Error /> },
-  { path: "*", element: <Error /> },
-  { path: "*", element: <Error /> },
-  { path: "*", element: <Error /> },
-  { path: "*", element: <Error /> },
-  { path: "*", element: <Error /> },
-  { path: "*", element: <Error /> },
-  { path: "*", element: <Error /> },
-  { path: "*", element: <Error /> },
-  { path: "*", element: <Error /> },
-  { path: "*", element: <Error /> },
-  { path: "*", element: <Error /> },
-  { path: "*", element: <Error /> },
-  { path: "*", element: <Error /> },
-  { path: "*", element: <Error /> },
-  { path: "*", element: <Error /> },
+  { path: "/courses", element: <Courses /> },
+  { path: "/courses/:id", element: <CoursDetalies /> },
+  { path: "/user/Booking", element: <Booking /> },
+  { path: "/login", element: <Login /> },
+  { path: "/forgetpassword", element: <ForgetPassword /> },
+  { path: "/otp", element: <OPT /> },
+  { path: "/signup", element: <SignUp /> },
+  { path: "", element: <div /> },
+  { path: "", element: <div /> },
+  { path: "", element: <div /> },
+  { path: "", element: <div /> },
+  { path: "", element: <div /> },
+  { path: "", element: <div /> },
+  { path: "", element: <div /> },
+  { path: "", element: <div /> },
+  { path: "", element: <div /> },
+  { path: "", element: <div /> },
+  { path: "", element: <div /> },
+  { path: "", element: <div /> },
+  { path: "", element: <div /> },
 
 
   
