a43e156e3b7c97ba389e7dfd6e94ba03d846f89f
hadeelalwadia <hadeelwadia@gmail.com>
2023-06-19 09:55:02 +0300

    create all thing specialized in get courses data from api

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,1 +1,21 @@
-export {}
+import axios, { Axios } from "axios";
+const token= document.cookie?document.cookie.split(";")[0].split('=')[0]:"";
+const baseURL='https://act-hub.actit.ps/api/auth/user/';
+const headers=token?{'Authorization':token}:{}
+const fetchData=new Axios({baseURL,headers})
+
+export const Api={
+ get:(url:string)=>{
+   return  fetchData.get(url)
+},
+
+post(){
+
+},
+put(){
+
+},
+delete(){
+
+}
+}
--- src/store/actions/Home.action.ts
+++ src/store/actions/Home.action.ts
@@ -1,1 +1,51 @@
-export {}
+import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from "axios";
+
+export const baseURL = "https://act-hub.actit.ps/api";
+
+const Axios: AxiosInstance = axios.create({
+  baseURL:baseURL,
+});
+
+Axios.interceptors.request.use(
+  (config:any) => {
+    const  token  =  localStorage.getItem("token")||  "" ;
+    if (token) {
+      config.headers = {
+        Authorization: `Bearer ${token}`,
+      };
+    }
+    return config;
+  },
+  (error:any) => {
+    Promise.reject(error);
+  }
+);
+
+export default class API {
+  static async get(
+    url: string,
+    config?: AxiosRequestConfig 
+  ): Promise<AxiosResponse> {
+    return Axios.get(url, config || {});
+  }
+
+  static async post<T>(
+    url: string,
+    body: T,
+    config?: AxiosRequestConfig
+  ): Promise<AxiosResponse> {
+    return Axios.post(url, body, config || {});
+  }
+
+  static async put<T>(url: string, body?: T): Promise<AxiosResponse> {
+    return Axios.put(url, body);
+  }
+  static async delete(
+    url: string,
+    config?: AxiosRequestConfig
+  ): Promise<AxiosResponse> {
+    return Axios.delete(url, config || {});
+  }
+}
+
+
