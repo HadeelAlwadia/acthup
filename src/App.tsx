e074688a7977e3a4a84c453be76a8a28f89d3051
hadeelalwadia <hadeelwadia@gmail.com>
2023-08-31 16:09:08 +0300

    update on the project

--- src/App.tsx
+++ src/App.tsx
@@ -11,22 +11,20 @@ import { storeDataInStorage } from './utils/functionlty';
 
 
 function App() {
-   const storeProvilder=createContext({
-      
-   })
+
    const { theme} = useSelector((state: AppState): AppState => state);
 
    useEffect(() => {
       storeDataInStorage('theme', theme);
    }, [theme])
    return (
-      <storeProvilder.Provider value={{courses:[]}}>
+      <>
          <ThemeProvider theme={theme === 'dark'?darkTheme:lightTheme }>
           <Header />
             <RouterProvider router={RootRoutes} />
          <Footer/>
          </ThemeProvider>
-      </storeProvilder.Provider>
+      </>
    );
 } 
 export default App;
